49552ff1ca903b208cae8aeaa4c22b7856b0219b
Liam Pugh <liam.pugh.009@gmail.com>
2022-05-15 22:40:50 +0100

    Added frame and layout

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+const (
+	screenWidth  = 800
+	screenHeight = 640
+	frameWidth   = 32
+	frameHeight  = 32
+)
+
 type Game struct{}
 
 func (g *Game) Update() error {
@@ -15,15 +22,19 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
+	paddle := &ebiten.DrawImageOptions{}
+	paddle.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
+	paddle.GeoM.Translate(screenWidth/2, screenHeight/2)
+
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Pong Game")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
